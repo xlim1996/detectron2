17f42e025f24a74226aba7a60cb576bd790f4d3f
Xiaolin Lin <xlim1996@outlook.com>
2023-05-18 11:26:23 +0200

    capture image and depth from zed camera

--- try_generate_seg_pcd.py
+++ try_generate_seg_pcd.py
@@ -1,21 +1,14 @@
-'''
-Author: Xiaolin Lin xlim1996@outlook.com
-Date: 2023-05-02 23:28:32
-LastEditors: Xiaolin Lin xlim1996@outlook.com
-LastEditTime: 2023-05-18 09:41:29
-FilePath: /hiwi/test_zed_1.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
+
 import time
 import torch
 import numpy as np
 import cv2
 import os
 import matplotlib.pyplot as plt
-import matplotlib.colors as mplc
-import matplotlib as mpl
 from PIL import Image
 import open3d as o3d
+
+import pyzed.sl as sl
 
 from detectron2.checkpoint import DetectionCheckpointer
 from detectron2.config import LazyConfig, instantiate
@@ -23,6 +16,8 @@ from detectron2.engine.defaults import create_ddp_model
 from detectron2.data import MetadataCatalog
 from detectron2.utils.visualizer import Visualizer,GenericMask
 from detectron2.structures.instances import Instances
+
+
 def cv2_imshow(img_bgr):
     plt.rcParams['figure.figsize'] = (18, 36)
     plt.axis('off')
@@ -55,116 +50,57 @@ file_list = os.listdir(file_path)
 rgb_result_path='./rgb_result/VItdet_new'
 save = True
 
+# Create a ZED camera
+zed = sl.Camera()
+# Create a InitParameters object and set configuration parameters
+init_params = sl.InitParameters()
+#TODO: change depth mode 
+init_params.depth_mode = sl.DEPTH_MODE.PERFORMANCE  # Use PERFORMANCE depth mode  
+init_params.coordinate_units = sl.UNIT.METER  # Use meter units (for depth measurements)
+init_params.camera_resolution = sl.RESOLUTION.HD720  # Use HD1080 video mode
+init_params.camera_fps = 30  # Set fps at 30
+calibration_params=zed.get_camera_information().camera_configuration.calibration_parameters
+fx=calibration_params.left_cam.fx
+fy=calibration_params.left_cam.fy
+cx=calibration_params.left_cam.cx
+cy=calibration_params.left_cam.cy
 
+#TODO: check extrinsic matrix
+rotation = calibration_params.get_rotation_matrix()
+translation = calibration_params.get_translation_vector()
+extrinsic_matrix = np.array([[rotation[0], rotation[1], rotation[2], translation[0]],
+                             [rotation[3], rotation[4], rotation[5], translation[1]],
+                             [rotation[6], rotation[7], rotation[8], translation[2]],
+                             [0, 0, 0, 1]])
 
-file_path='ZED_camera_data'
-file_name = 'DepthViewer_Left_29934236_1242_02-05-2023-12-44-38.png'
-pcd_name = 'point_cloud_PCD_29934236_1242_02-05-2023-12-44-42.pcd'
-with torch.inference_mode():
-    
-    pcd = o3d.io.read_point_cloud(os.path.join(file_path, pcd_name))
-    o3d.visualization.draw_geometries([pcd])
-    image = cv2.imread(os.path.join(file_path, file_name))
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    H_o, W_o = image.shape[:2]
-    image = cv2.resize(image,(int(W_o / 2), int(H_o / 2)))
-    image = np.array(image, dtype=np.uint8)
-    H, W = image.shape[:2]
-    image_model = np.moveaxis(image, -1, 0)        
-    time_start = time.time()
-    output = model([{'image': torch.from_numpy(image_model)}])
-    time_end = time.time()
-    print('time cost', time_end - time_start, 's')
-    time_start = time.time()
-    result=[]
-    if "instances" in output[0]:
-        instances = output[0]["instances"].to("cpu")
-        for i in range(len(instances.pred_classes)):
-            if instances.pred_classes[i]+1 in id:
-                result.append(instances[i])
-        result=Instances.cat(result)
-    time_end = time.time()
-    print('time cost', time_end - time_start, 's')
+# Open the camera
+err = zed.open(init_params)
+if err != sl.ERROR_CODE.SUCCESS:
+    exit(1)
+#capture image and depth from zed camera
+image = sl.Mat()
+depth = sl.Mat()
+runtime_parameters = sl.RuntimeParameters()
+# Grab an image, a RuntimeParameters object must be given to grab()
+if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+    # A new image is available if grab() returns SUCCESS
+    zed.retrieve_image(image, sl.VIEW.LEFT)
+    zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
+# Close the camera
+zed.close()
+#TODO: check the depth range
 
-    v = Visualizer(image[:, :, ::-1], MetadataCatalog.get(cfg.dataloader.train.dataset.names), scale=1.2)
-    # v = Visualizer(image_show[:, :, ::-1], metadata, scale=1.2)
-    v = v.draw_instance_predictions(result.to("cpu"))
-    output = v.get_image()[:, :, ::-1]
-    print("output",output.shape)
-    cv2_imshow(output)
-    #resize image back to original size
-    image_result = cv2.resize(output, (W_o, H_o))
-    '''
-Author: Xiaolin Lin xlim1996@outlook.com
-Date: 2023-05-02 23:28:32
-LastEditors: Xiaolin Lin xlim1996@outlook.com
-LastEditTime: 2023-05-05 11:15:17
-FilePath: /hiwi/test_zed_1.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
-import time
-import torch
-import numpy as np
-import cv2
-import os
-import matplotlib.pyplot as plt
-import matplotlib.colors as mplc
-import matplotlib as mpl
-from PIL import Image
-import open3d as o3d
-
-from detectron2.checkpoint import DetectionCheckpointer
-from detectron2.config import LazyConfig, instantiate
-from detectron2.engine.defaults import create_ddp_model
-from detectron2.data import MetadataCatalog
-from detectron2.utils.visualizer import Visualizer,GenericMask
-from detectron2.structures.instances import Instances
-def cv2_imshow(img_bgr):
-    plt.rcParams['figure.figsize'] = (18, 36)
-    plt.axis('off')
-    print(img_bgr.shape)
-    print(img_bgr[...,::-1].shape)
-    plt.imshow(img_bgr[...,::-1])
-    plt.show()
-
-
-cfg = LazyConfig.load('projects/ViTDet/configs/LVIS_MIX/cascade_mask_rcnn_vitdet_h_100ep.py')
-metadata = MetadataCatalog.get(cfg.dataloader.train.dataset.names) # to get labels from ids
-classes = metadata.thing_classes
-
-id=[]
-for i in range(len(classes)):
-    id.append(metadata.get('class_image_count')[i]['id'])
-cfg = LazyConfig.load('projects/ViTDet/configs/LVIS/cascade_mask_rcnn_vitdet_h_100ep.py')
-cfg.train.init_checkpoint = "checkpoints/ViTDet/model_final_11bbb7.pkl" # replace with the path were you have your model
-
-
-model = instantiate(cfg.model)
-model.to(cfg.train.device)
-model = create_ddp_model(model)
-DetectionCheckpointer(model).load(cfg.train.init_checkpoint)
-
-model.eval()
-print("model loaded",model)
-file_path = 'ZED_camera_data'
-file_list = os.listdir(file_path)
-rgb_result_path='./rgb_result/VItdet_new'
-save = True
-
-
-
-file_path='ZED_camera_data'
-file_name = 'DepthViewer_Left_29934236_1242_02-05-2023-12-44-38.png'
-pcd_name = 'point_cloud_PCD_29934236_1242_02-05-2023-12-44-42.pcd'
 with torch.inference_mode():
     
     # pcd = o3d.io.read_point_cloud(os.path.join(file_path, pcd_name))
     # o3d.visualization.draw_geometries([pcd])
-    #capture image from zed camera
-    image = cv2.VideoCapture(0)
+    
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     H_o, W_o = image.shape[:2]
+    #TODO: need to change the image shape to decide whether to resize or not
     #resize image to half size(solve the problem of detectron2 input size)
+    # H_detect = 800
+    # W_detect = 1280
     image = cv2.resize(image,(int(W_o / 2), int(H_o / 2)))
     #convert data type of image
     image = np.array(image, dtype=np.uint8)
@@ -196,14 +132,7 @@ with torch.inference_mode():
     image_result = cv2.resize(output, (W_o, H_o))
 
     #instance segmentation result to point cloud
-    intrinsic_matrix = np.array([[fx, 0, cx],
-                             [0, fy, cy],
-                             [0, 0, 1]])
-    extrinsic_matrix = np.array([[r11, r12, r13, tx],
-                                [r21, r22, r23, ty],
-                                [r31, r32, r33, tz],
-                                [0, 0, 0, 1]])
-    depth=None
+    intrinsic_matrix = o3d.camera.PinholeCameraIntrinsic(width=W_o, height=H_o, fx=fx, fy=fy, cx=cx, cy=cy)
 
     rgbd_image=o3d.geometry.create_rgbd_image_from_color_and_depth(image_result,depth,depth_scale=1.0,depth_trunc=3.0,convert_rgb_to_intensity=False)
     pcd = o3d.geometry.PointCloud.create_from_rgbd_image(
